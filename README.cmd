14985bebaf02ea0f3fb8767f1a25449d39f6f482
Andreas Schmittner <aschmitt@coas.oregonstate.edu>
2017-08-29 17:36:36 -0700

    instructions added

--- README.cmd
+++ README.cmd
@@ -1,2 +1,4 @@
 This is a working directory of UVic2.9/MOBI2.0 with flux corrected advection (fct), the basic npzd (nutrients, phytoplankton, zooplankton, detritus) ecosystem dynamics (npzd), oxygen (o2), nitrogen (n), iron (fe), carbon and alkalinity (ca) and calcium carbonate (caco3). See mk.in.
+
+For this to work you need the base UVic model installation with the most recent updates available at https://github.com/OSU-CEOAS-Schmittner/UVic2.9.
  
